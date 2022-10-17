6ca8c3454a1c94afa6abdd5cb0bdb0ef78d2fac6
Timur Tripp <timur@timurtripp.com>
2022-10-17 10:20:17 -0600

    Passes variables to JavaScript

--- js/cuboulder-today-filter-form-loader.js
+++ js/cuboulder-today-filter-form-loader.js
@@ -1,8 +1,21 @@
-function cuboulderTodayFilterFormLoader(filterName, filterConfig) {
+function cuboulderTodayFilterFormLoader(siteURI, globalURI, label, path, machineName, configuration) {
+	let loaded = false;
+	function __loadcuboulderTodayFilterForm() {
+		console.log('Load function called.');
+		loaded = true;
+	}
 	const
-		containerElement = document.getElementById('cuboulder_today_filter_form_container_' + filterName),
-		checkboxElement = containerElement.querySelector('.js-form-type-checkbox'),
-		checkboxesElement = checkboxElement.parentElement,
-		checkboxElementHTML = checkboxElement.innerHTML; // Stores the HTML of a checkbox to duplicate it later
+		containerElement = document.getElementById('cuboulder_today_filter_form_container_' + machineName),
+		checkboxesElement = containerElement.querySelector('.form-checkboxes'),
+		checkboxElementHTML = checkboxesElement.innerHTML; // Stores the HTML of a checkbox to duplicate it later
 	checkboxesElement.innerHTML = '';
+
+	if(configuration['enabled'])
+		__loadcuboulderTodayFilterForm();
+	else {
+		document.querySelector('.cuboulder-today-filter-form-show-all-' + machineName).addEventListener('change', function(event) {
+			if(!loaded && event.currentTarget.checked)
+				__loadcuboulderTodayFilterForm();
+		});
+	}
 }
