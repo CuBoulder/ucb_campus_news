11d21b92a4c4552cb0eb85cc8dcd8eccdfb77a03
Timur Tripp <timur@timurtripp.com>
2022-10-18 08:46:20 -0600

    Adds functions to begin processing filter API data

--- js/cuboulder-today-filter-form-loader.js
+++ js/cuboulder-today-filter-form-loader.js
@@ -1,21 +1,50 @@
-function cuboulderTodayFilterFormLoader(siteURI, globalURI, label, path, machineName, configuration) {
-	let loaded = false;
-	function __loadcuboulderTodayFilterForm() {
-		console.log('Load function called.');
-		loaded = true;
-	}
+function cuboulderTodayFilterFormLoader(siteURI, baseURI, label, path, machineName, configuration) {
 	const
 		containerElement = document.getElementById('cuboulder_today_filter_form_container_' + machineName),
 		checkboxesElement = containerElement.querySelector('.form-checkboxes'),
 		checkboxElementHTML = checkboxesElement.innerHTML; // Stores the HTML of a checkbox to duplicate it later
-	checkboxesElement.innerHTML = '';
-
+	let loadRequested = false, loadComplete = false;
+	function _buildTree(data) {
+		const root = {id: 0, children: []}, tidMap = new Map();
+		tidMap.set(0, root);
+		data.sort((itemA, itemB) => itemA['depth'] - itemB['depth']).forEach((item) => {
+			const tid = item['tid'], node = {
+				id: tid,
+				uuid: item['uuid'],
+				name: item['name'],
+				description: item['description'],
+				weight: item['weight'],
+				children: []
+			};
+			tidMap.set(tid, node);
+			item['parents'].forEach((parentId) => {
+				const parentNode = tidMap.get(parentId);
+				if(parentNode)
+					parentNode.children.push(node);
+			});
+		});
+		return root;
+	}
+	function _load() {
+		loadRequested = true;
+		const loaderDiv = document.createElement('div');
+		loaderDiv.innerHTML = '<div class="ajax-progress ajax-progress-throbber"><div class="throbber"></div></div>';
+		containerElement.insertBefore(loaderDiv, containerElement.childNodes[0]);
+		fetch(baseURI + path)
+			.then((response) => response.json())
+			.then((data) => _buildTree(data))
+			.then((tree) => {
+				console.log(tree);
+				containerElement.removeChild(loaderDiv);
+				loadComplete = true;
+			});
+	}
 	if(configuration['enabled'])
-		__loadcuboulderTodayFilterForm();
+		_load();
 	else {
 		document.querySelector('.cuboulder-today-filter-form-show-all-' + machineName).addEventListener('change', function(event) {
-			if(!loaded && event.currentTarget.checked)
-				__loadcuboulderTodayFilterForm();
+			if(!loadRequested && event.currentTarget.checked)
+				_load();
 		});
 	}
 }
